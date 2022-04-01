5f23548c7d9c2c8e87899be9dbd1cdfe61516cb9
nsandau <12936029+nsandau@users.noreply.github.com>
2022-04-01 09:22:25 +0200

    add cli

--- cronscript.sh
+++ cronscript.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
+#!/bin/bash
+cd /home/dietpi/aescrape
 docker run --rm -w /usr/workspace -v $(pwd):/usr/workspace aedocker python ae.py --country "Kroatien" --city "Split" --pickup "Split Airport" --pickupdate 29-JUNI-2022 --pickuptime 14 --dropoffdate 6-JULI-2022 --dropofftime 15
